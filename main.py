27d5251b58fda78b7300c304fccde468454ff887
rupak-20 <rupakswami43887@gmail.com>
2022-06-07 19:35:22 +0530

    minor code improvements and bug fixes

--- main.py
+++ main.py
@@ -5,14 +5,22 @@ from dotenv import load_dotenv
 if __name__ == '__main__':
 
     load_dotenv()
-    img_path, import_fbx_path, export_fbx_path = sys.argv[1], sys.argv[2], sys.argv[3]
-    blender_path = os.getenv('BLENDER_PATH')
-    obj_path = os.getenv('MEDIAPIPE_OBJ_NAME')
-    
-    os.system('python image_to_obj.py ' + img_path + ' ' + obj_path)
-    print('++++ Part 1: Mediapipe mesh obtained from pose image')
+    if len(sys.argv) < 4:
+        print('expected 3 additional arguments, passed', len(sys.argv) - 1)
+        print('expected python main.py <IMG_PATH> <INPUT_FBX_PATH> <OUTPUT_FBX_PATH>')
 
-    blender_invoke_command = '"' + blender_path + '"' + ' --background --python ' + 'obj_to_armature.py ' + import_fbx_path + ' ' + export_fbx_path + ' ' + obj_path
-    print(blender_invoke_command)
-    os.system(blender_invoke_command)
-    print('++++ Part 2: Posed armature obtained from Mediapipe mesh.')
+    elif len(sys.argv) > 4:
+        print('expected 3 additional arguments, passed', len(sys.argv) - 1)
+        print('expected python main.py <IMG_PATH> <INPUT_FBX_PATH> <OUTPUT_FBX_PATH>')
+
+    else:
+        img_path, import_fbx_path, export_fbx_path = sys.argv[1], sys.argv[2], sys.argv[3]
+        blender_path = os.getenv('BLENDER_PATH')
+        obj_path = 'mediapipe_mesh.obj'
+        
+        os.system('python image_to_obj.py ' + img_path + ' ' + obj_path)
+        print('++++ Part 1: Mediapipe mesh obtained from ' + img_path)
+
+        blender_invoke_command = '"' + blender_path + '"' + ' --background --python ' + 'obj_to_armature.py ' + import_fbx_path + ' ' + export_fbx_path + ' ' + obj_path
+        os.system(blender_invoke_command)
+        print('++++ Part 2: Posed FBX written to ' + export_fbx_path)
